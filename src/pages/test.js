afce2301413fbef89b2da8c09d57f39d0e4937eb
NickBonet <nick.b98@hotmail.com>
2021-04-11 13:15:37 -0400

    Setup Material-UI, ESLint.

---
+++ src/pages/test.js
@@ -0,0 +1,9 @@
+import Button from '@material-ui/core/Button';
+export default function Test() {
+    return ( 
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+    )
+}
+
