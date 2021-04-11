17046e8e6d3a4f852bfe55fa63524a2faa368cd0
NickBonet <nick.b98@hotmail.com>
2021-04-11 19:51:34 -0400

    Add boilerplate for basic header bar for the app.

---
+++ src/components/layout.js
@@ -0,0 +1,40 @@
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Layout({ children }) {
+  const classes = useStyles()
+
+  return ( 
+    <div>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        <Typography variant="h6" color="inherit">
+          TCG Card Search App
+        </Typography>
+        </Toolbar>
+      </AppBar>
+
+      {children}
+    </div>
+  )
+}
+  
--- src/pages/card/[cardId].js
+++ src/pages/card/[cardId].js
@@ -1,37 +1,39 @@
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Layout from '../../components/layout'
 import Head from 'next/head'
 import pokemon from 'pokemontcgsdk'
 
 function Test({ res }) {
     return (
-      <Container maxWidth="xs" style={{textAlign: "center"}}>
-        <Head>
-          <title>TCG Card View</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head> 
+      <Layout>
+        <Container maxWidth="xs" style={{textAlign: "center"}}>
+          <Head>
+            <title>TCG Card View</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head> 
 
-        <Typography variant="h3">
-          TCG Card Page Example
-        </Typography>
+          <Typography variant="h3">
+            TCG Card Page Example
+          </Typography>
 
-        <Typography variant="h5">
-          Card Name: {res.name}
-          <br />
-          Set: {res.set.name} <img src={res.set.images.symbol} width="10%" height="10%" />
-          <br />
-          {res.number}/{res.set.printedTotal} 
-        </Typography>
+          <Typography variant="h5">
+            Card Name: {res.name}
+            <br />
+            Set: {res.set.name} <img src={res.set.images.symbol} width="10%" height="10%" />
+            <br />
+            {res.number}/{res.set.printedTotal} 
+          </Typography>
 
-        <img src={res.images.small} />
-      </Container>
+          <img src={res.images.small} />
+        </Container>
+      </Layout>
     )
 }
 
 export const getServerSideProps = async (context) => {
   const cardId = context.params.cardId
   const res = await pokemon.card.find(cardId)
-  console.log(res)
 
   return { props: { res } }
 }
