ac0bd651431323c1d1d5d90a94dae6cf3fe231dc
NickBonet <nick.b98@hotmail.com>
2021-04-21 22:58:40 -0400

    Basic search page framework impl.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,13 @@ import { Typography, Grid } from "@material-ui/core";
 import SearchBar from "material-ui-search-bar";
 import Head from "next/head";
 import Layout from "../components/layout";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const [searchVal, setSearchVal] = useState("");
+
   return (
     <Layout>
       <Head>
@@ -15,19 +20,24 @@ export default function Home() {
         direction="column"
         align="center"
         justify="center"
+        spacing={1}
         style={{ minHeight: "90vh" }}
       >
         <Grid item container direction="column" display="flex">
           <Typography variant="h2">Welcome to TCG Card Search!</Typography>
+        </Grid>
+        <Grid item container direction="column" display="flex">
           <Typography variant="h5">
             Start entering your query in the search box below to get started.
           </Typography>
         </Grid>
-        <br />
-        <Grid item container direction="column" display="flex" justify="center">
+        <Grid item container direction="column" display="flex">
           <SearchBar
             placeholder="Search for a card"
+            value={searchVal}
             cancelOnEscape="true"
+            onChange={(newVal) => setSearchVal(newVal)}
+            onRequestSearch={() => router.push(`/search/${searchVal}`)}
             style={{ margin: "0 auto", minWidth: "35vw" }}
           />
         </Grid>
---
+++ src/pages/search/[searchParams].js
@@ -0,0 +1,55 @@
+import pokemon from "pokemontcgsdk";
+import Layout from "../../components/layout";
+import { Grid, Typography } from "@material-ui/core";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+function Search({ res }) {
+  const router = useRouter();
+  const query = router.query.searchParams;
+  const cardList = [];
+  var i;
+
+  for (i = 0; i < res.data.length; i++) {
+    cardList.push(
+      <Grid item display="flex" align="center">
+        <img src={res.data[i].images.small} />
+      </Grid>
+    );
+  }
+
+  return (
+    <Layout>
+      <Head>
+        <title>
+          Search results for: {query[0].toUpperCase() + query.slice(1)}
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Grid
+        container
+        align="center"
+        justify="center"
+        spacing={2}
+        style={{ minHeight: "90vh" }}
+      >
+        <Grid item container direction="column" display="flex">
+          <Typography variant="h3">
+            Search results for: {query[0].toUpperCase() + query.slice(1)}
+          </Typography>
+        </Grid>
+        {cardList}
+      </Grid>
+    </Layout>
+  );
+}
+
+export const getServerSideProps = async (context) => {
+  const searchParams = context.params.searchParams;
+  const res = await pokemon.card.where({ q: `name:${searchParams}` });
+  console.log(res.data.length);
+
+  return { props: { res } };
+};
+
+export default Search;
