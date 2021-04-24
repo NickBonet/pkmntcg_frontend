82fae33b940c3a639e1f5e2f25523a88ebb6a73c
NickBonet <nick.b98@hotmail.com>
2021-04-24 18:08:27 -0400

    Fix helper method

--- src/helpers.js
+++ src/helpers.js
@@ -2,5 +2,5 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 // Very rudimentary function to scale certain UI elements for mobile.
 export function isMobile(theme) {
-  return useMediaQuery(theme.breakpoints.down("md"));
+  return useMediaQuery(theme.breakpoints.down("sm"));
 }
